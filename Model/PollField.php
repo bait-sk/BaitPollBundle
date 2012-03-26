389b91abd1ab6ede78385a5cc4e00e7b9b335ac8
Ondrej Slinták <ondrowan@gmail.com>
2012-03-26 20:20:55 +0200

    Added PollField model and entity

---
+++ Model/PollField.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace Bait\PollBundle\Model;
+
+/**
+ * Base PollQuestion object
+ *
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+abstract class PollField
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $createdBy;
+
+    /**
+     * @var datetime
+     */
+    protected $createdAt;
+
+    /**
+     * @var datetime
+     */
+    protected $modifiedBy;
+
+    /**
+     * @var datetime
+     */
+    protected $modifiedAt;
+
+    /**
+     * @var boolean
+     */
+    protected $isActive;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Returns unique id of poll question.
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets title of poll question.
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Gets title of poll question.
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Sets owner of poll question.
+     *
+     * @param mixed $createdBy
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Gets owner of poll question.
+     *
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Gets date of creation of poll question.
+     *
+     * @return datetime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Sets who modified the poll question.
+     *
+     * @param mixed $modifiedBy
+     */
+    public function setModifiedBy($modifiedBy)
+    {
+        $this->modifiedBy = $modifiedBy;
+
+        return $this;
+    }
+
+    /**
+     * Gets who modified the poll question.
+     *
+     * @return mixed
+     */
+    public function getModifiedBy()
+    {
+        return $this->modifiedBy;
+    }
+
+    /**
+     * Sets date of modification of poll question.
+     *
+     * @param datetime $modifiedAt
+     */
+    public function setModifiedAt(\DateTime $modifiedAt)
+    {
+        $this->modifiedAt = $modifiedAt;
+
+        return $this;
+    }
+
+    /**
+     * Gets date of modification of poll question.
+     *
+     * @return datetime
+     */
+    public function getModifiedAt()
+    {
+        return $this->modifiedAt;
+    }
+
+    /**
+     * Sets whether poll question is active or not.
+     *
+     * @param boolean $isActive
+     */
+    public function setActive($isActive)
+    {
+        $this->isActive = (boolean) $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Gets whether poll question is active or not.
+     *
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->isActive;
+    }
+}
---
+++ Tests/Model/PollFieldTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Bait\PollBundle\Tests\Model;
+
+use Bait\PollBundle\Model\Poll;
+
+class PollFieldTest extends \PHPUnit_Framework_TestCase
+{
+    protected $pollQuestion;
+
+    protected function setUp()
+    {
+        $this->pollQuestion = $this->getMockForAbstractClass('Bait\PollBundle\Model\PollField');
+    }
+
+    public function testTitle()
+    {
+        $this->assertNull($this->pollQuestion->getTitle());
+
+        $this->pollQuestion->setTitle('Poll #1');
+        $this->assertEquals('Poll #1', $this->pollQuestion->getTitle());
+    }
+
+    public function testCreatedBy()
+    {
+        $this->assertNull($this->pollQuestion->getCreatedBy());
+
+        $this->pollQuestion->setCreatedBy('stewie');
+        $this->assertEquals('stewie', $this->pollQuestion->getCreatedBy());
+    }
+
+    public function testCreatedAt()
+    {
+        $this->assertEquals(new \DateTime(), $this->pollQuestion->getCreatedAt());
+    }
+
+    public function testModifiedBy()
+    {
+        $this->assertNull($this->pollQuestion->getModifiedBy());
+
+        $this->pollQuestion->setModifiedBy('stewie');
+        $this->assertEquals('stewie', $this->pollQuestion->getModifiedBy());
+    }
+
+    /**
+     * @expectedException ErrorException
+     */
+    public function testModifiedAtDataType()
+    {
+        $this->pollQuestion->setModifiedAt('1991-08-27');
+    }
+
+    public function testModifiedAt()
+    {
+        $this->assertNull($this->pollQuestion->getModifiedAt());
+
+        $date = new \DateTime('1991-08-27');
+
+        $this->pollQuestion->setModifiedAt($date);
+        $this->assertEquals($date, $this->pollQuestion->getModifiedAt());
+    }
+}
