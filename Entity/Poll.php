39d3a98b85e298ec943f02ca40e4807b4ee3b0b4
Ondrej Slinták <ondrowan@gmail.com>
2012-03-26 15:26:44 +0200

    Added base ORM entity for Poll

---
+++ Entity/Poll.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bait\PollBundle\Entity;
+
+use Bait\PollBundle\Model\Poll as AbstractUser;
+
+/**
+ * Base "poll" Doctrine 2 entity
+ *
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+abstract class Poll extends AbstractUser
+{
+}
