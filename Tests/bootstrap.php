3d0c0da5f4889fbc407921a5da6077d9f3b347de
Ondrej Slinták <ondrowan@gmail.com>
2012-04-03 22:12:28 +0200

    Initialized Symfony error handler in tester bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
+$vendorDir = __DIR__ . '/../vendor';
+
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
 } elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
     require_once $file;
 }
+
+Symfony\Component\HttpKernel\Debug\ErrorHandler::register();
