857b1701f7b1c756eaf149d9daccc006c1ae3d7b
ondrowan <ondrowan@gmail.com>
2012-04-05 13:29:48 +0200

    Removed author fields from PollField

--- Model/PollField.php
+++ Model/PollField.php
@@ -20,24 +20,9 @@ abstract class PollField
     protected $title;
 
     /**
-     * @var string
-     */
-    protected $createdBy;
-
-    /**
-     * @var datetime
+     * @var \DateTime
      */
     protected $createdAt;
-
-    /**
-     * @var datetime
-     */
-    protected $modifiedBy;
-
-    /**
-     * @var datetime
-     */
-    protected $modifiedAt;
 
     /**
      * @var boolean
@@ -78,79 +63,13 @@ abstract class PollField
     }
 
     /**
-     * Sets owner of poll question.
-     *
-     * @param mixed $createdBy
-     */
-    public function setCreatedBy($createdBy)
-    {
-        $this->createdBy = $createdBy;
-
-        return $this;
-    }
-
-    /**
-     * Gets owner of poll question.
-     *
-     * @return mixed
-     */
-    public function getCreatedBy()
-    {
-        return $this->createdBy;
-    }
-
-    /**
      * Gets date of creation of poll question.
      *
-     * @return datetime
+     * @return \DateTime
      */
     public function getCreatedAt()
     {
         return $this->createdAt;
-    }
-
-    /**
-     * Sets who modified the poll question.
-     *
-     * @param mixed $modifiedBy
-     */
-    public function setModifiedBy($modifiedBy)
-    {
-        $this->modifiedBy = $modifiedBy;
-
-        return $this;
-    }
-
-    /**
-     * Gets who modified the poll question.
-     *
-     * @return mixed
-     */
-    public function getModifiedBy()
-    {
-        return $this->modifiedBy;
-    }
-
-    /**
-     * Sets date of modification of poll question.
-     *
-     * @param datetime $modifiedAt
-     */
-    public function setModifiedAt(\DateTime $modifiedAt)
-    {
-        $this->modifiedAt = $modifiedAt;
-
-        return $this;
-    }
-
-    /**
-     * Gets date of modification of poll question.
-     *
-     * @return datetime
-     */
-    public function getModifiedAt()
-    {
-        return $this->modifiedAt;
     }
 
     /**
--- Tests/Model/PollFieldTest.php
+++ Tests/Model/PollFieldTest.php
@@ -19,42 +19,8 @@ class PollFieldTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('Poll #1', $this->pollQuestion->getTitle());
     }
 
-    public function testCreatedBy()
-    {
-        $this->assertNull($this->pollQuestion->getCreatedBy());
-
-        $this->pollQuestion->setCreatedBy('stewie');
-        $this->assertEquals('stewie', $this->pollQuestion->getCreatedBy());
-    }
-
     public function testCreatedAt()
     {
         $this->assertEquals(new \DateTime(), $this->pollQuestion->getCreatedAt());
-    }
-
-    public function testModifiedBy()
-    {
-        $this->assertNull($this->pollQuestion->getModifiedBy());
-
-        $this->pollQuestion->setModifiedBy('stewie');
-        $this->assertEquals('stewie', $this->pollQuestion->getModifiedBy());
-    }
-
-    /**
-     * @expectedException ErrorException
-     */
-    public function testModifiedAtDataType()
-    {
-        $this->pollQuestion->setModifiedAt('1991-08-27');
-    }
-
-    public function testModifiedAt()
-    {
-        $this->assertNull($this->pollQuestion->getModifiedAt());
-
-        $date = new \DateTime('1991-08-27');
-
-        $this->pollQuestion->setModifiedAt($date);
-        $this->assertEquals($date, $this->pollQuestion->getModifiedAt());
     }
 }
