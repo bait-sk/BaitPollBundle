02ac25fe5a03d05bcddee642b916ca490070c1df
ondrowan <ondrowan@gmail.com>
2012-04-05 10:03:53 +0200

    Added ORM entity for PollGroup

---
+++ Entity/PollGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bait\PollGroupBundle\Entity;
+
+use Bait\PollGroupBundle\Model\PollGroup as PollGroupModel;
+
+/**
+ * Base PollGroup ORM entity
+ *
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+abstract class PollGroup extends PollGroupModel
+{
+}
