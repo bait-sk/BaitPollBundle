23262385f0efe4bd97506662d5b790a93edb127a
ondrowan <ondrowan@gmail.com>
2012-04-05 10:03:41 +0200

    Added ORM entity for PollField

---
+++ Entity/PollField.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bait\PollBundle\Entity;
+
+use Bait\PollBundle\Model\Poll as PollModel;
+
+/**
+ * Base Poll ORM entity
+ *
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+abstract class Poll extends PollModel
+{
+}
