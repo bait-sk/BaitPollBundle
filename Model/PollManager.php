5ed545dd58c401b8208f24bc224f169f5fbef1b8
Ondrej Slinták <ondrowan@gmail.com>
2012-04-09 22:30:44 +0200

    Prepared simple schema agnostic Poll Manager

---
+++ Model/PollManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bait\PollBundle\Model;
+
+/**
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+class PollManager
+{
+    /**
+     * @param string $id
+     *
+     * @return PollInterface
+     */
+    public function findPollById($id)
+    {
+        return $this->findPollById(array('id' => $id));
+    }
+}
