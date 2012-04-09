1822d25859ca9a50e71c2ce3af0a8fce812f7263
Ondrej Slinták <ondrowan@gmail.com>
2012-04-09 23:10:10 +0200

    Prepared dummy PollField managers

---
+++ Entity/PollFieldManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bait\PollFieldBundle\Entity;
+
+use Doctrine\ORM\EntityManager;
+use Bait\PollFieldBundle\Model\PollFieldInterface;
+use Bait\PollFieldBundle\Model\PollFieldManager as BasePollFieldManager;
+
+/**
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+class PollFieldManager extends BasePollFieldManager
+{
+}
---
+++ Model/PollFieldManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bait\PollBundle\Model;
+
+/**
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+class PollFieldManager
+{
+}
