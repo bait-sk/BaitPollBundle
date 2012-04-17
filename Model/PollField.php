e049a2d2079384419d95143b771db0a9ca7eda60
ondrowan <ondrowan@gmail.com>
2012-04-17 12:13:04 +0200

    Reorganized poll field types

--- Model/PollField.php
+++ Model/PollField.php
@@ -10,9 +10,14 @@ namespace Bait\PollBundle\Model;
 abstract class PollField
 {
     const TYPE_INPUT = 'TYPE_INPUT';
+    const TYPE_INTEGER = 'TYPE_INTEGER';
     const TYPE_TEXTAREA = 'TYPE_TEXTAREA';
-    const TYPE_RADIO = 'TYPE_RADIO';
     const TYPE_FILE = 'TYPE_FILE';
+
+    const TYPE_RADIO = 'TYPE_RADIO';
+    const TYPE_CHECKBOX = 'TYPE_CHECKBOX';
+    const TYPE_DROPDOWN = 'TYPE_DROPDOWN';
+    const TYPE_DROPDOWN_MULTIPLE = 'TYPE_DROPDOWN_MULTIPLE';
 
     /**
      * @var mixed
