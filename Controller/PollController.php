75ffad017c1d32005f8f8819393743c07ec1c20c
ondrowan <ondrowan@gmail.com>
2012-04-19 11:38:25 +0200

    Added parent request to controller, so we can properly post to current url

--- Controller/PollController.php
+++ Controller/PollController.php
@@ -7,13 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PollController extends Controller
 {
-    public function showAction($id)
+    public function showAction(Request $request, Request $parentRequest, $id)
     {
         $form = $this->container->get('bait_poll.form_factory')->createForm($id);
+
+        if ($request->getMethod() === 'POST') {
+            $form->bindRequest($request);
+        }
 
         return $this->render('BaitPollBundle:Poll:show.html.twig', array(
             'id' => $id,
             'form' => $form->createView(),
+            'parentRequest' => $parentRequest,
         ));
     }
 
