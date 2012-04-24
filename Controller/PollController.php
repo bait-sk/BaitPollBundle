82449dbe24cf682b599cfa2b81b04b5a6aa93031
ondrowan <ondrowan@gmail.com>
2012-04-24 10:20:46 +0200

    Added some basic logic to controller

--- Controller/PollController.php
+++ Controller/PollController.php
@@ -15,7 +15,23 @@ class PollController extends Controller
             $form->bindRequest($request);
 
             if ($form->isValid()) {
-                // to be some logic... some day
+                $voteManager = $this->container->get('bait_poll.vote.manager');
+                $poll = $this->container->get('bait_poll.poll.manager')->findOneById($id);
+
+                $data = $form->getData();
+
+                $votes = array();
+
+                foreach ($data as $fieldId => $value) {
+                    $field = str_replace('field_', '', $fieldId);
+
+                    $field = $this->get('doctrine.orm.entity_manager')->getReference('AcmeDemoBundle:PollField', $field);
+                    $vote = $voteManager->create($field, $value);
+
+                    $votes[] = $vote;
+                }
+
+                $voteManager->save($votes);
             }
         }
 
