d96664c500b6d241ddbf72e4accbf604758a7f91
Ondrej Slinták <ondrowan@gmail.com>
2012-04-29 15:09:16 +0200

    Added license everywhere

--- BaitPollBundle.php
+++ BaitPollBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the BaitPollBundle package.
+ *
+ * (c) BAIT s.r.o. <http://www.bait.sk/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Bait\PollBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the BaitPollBundle package.
+ *
+ * (c) BAIT s.r.o. <http://www.bait.sk/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $vendorDir = __DIR__ . '/../vendor';
 
 if (file_exists($file = __DIR__.'/autoload.php')) {
