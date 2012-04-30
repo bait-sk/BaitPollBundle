640b4dc52419ec200504c3109cc903305fa8e7e0
ondrowan <ondrowan@gmail.com>
2012-04-30 16:36:17 +0200

    Added integer field

--- Form/PollType.php
+++ Form/PollType.php
@@ -38,6 +38,7 @@ class PollType extends AbstractType
     protected $fieldTypes = array(
         FieldInterface::TYPE_INPUT => 'text',
         FieldInterface::TYPE_EMAIL => 'email',
+        FieldInterface::TYPE_INTEGER => 'integer',
         FieldInterface::TYPE_RADIO => 'choice',
         FieldInterface::TYPE_CHECKBOX => 'choice',
         FieldInterface::TYPE_SELECT => 'choice',
