9efff36af495678874bb2fac331e45062f24365f
ondrowan <ondrowan@gmail.com>
2012-06-14 10:23:02 +0200

    Added signed interface for votes

---
+++ Model/SignedVoteInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the BaitPollBundle package.
+ *
+ * (c) BAIT s.r.o. <http://www.bait.sk/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bait\PollBundle\Model;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Inferface to be used in conjunction with FOSUserBundle.
+ *
+ * @author Ondrej Slintak <ondrowan@gmail.com>
+ */
+interface SignedVoteInterface extends PollInterface
+{
+    /**
+     * Sets author of the Poll
+     *
+     * @param UserInterface $user
+     */
+    function setAuthor(UserInterface $author);
+
+    /**
+     * Gets author of the Poll
+     *
+     * @return UserInterface
+     */
+    function getAuthor();
+}
