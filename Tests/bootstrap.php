f5cf8124af927e17535edee66747071271c1e788
Ondrej Slinták <ondrowan@gmail.com>
2012-08-10 11:54:31 +0200

    Updated composer.json and testing bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,12 +9,10 @@
  * file that was distributed with this source code.
  */
 
-$vendorDir = __DIR__ . '/../vendor';
-
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
+
+require $autoloadFile;
 
 Symfony\Component\HttpKernel\Debug\ErrorHandler::register();
